a74c5d6b0053fccf6bef6c2a7c77657481d6a338
pvshake <pvshakeu@gmail.com>
2023-10-04 17:07:51 -0300

    feito até 4:00:00

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -39,7 +39,11 @@ const ThreadCard = ({
   isComment,
 }: Props) => {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 felx-row gap-4">
           <div className="flex flex-col items-center">
@@ -68,7 +72,7 @@ const ThreadCard = ({
               <div className="flex gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
-                  alt="Gray heart"
+                  alt="heart"
                   width={24}
                   height={24}
                   className="cursor-pointer object-contain"
@@ -76,7 +80,7 @@ const ThreadCard = ({
                 <Link href={`/thread/${id}`}>
                   <Image
                     src="/assets/reply.svg"
-                    alt="reply"
+                    alt="heart"
                     width={24}
                     height={24}
                     className="cursor-pointer object-contain"
@@ -84,14 +88,14 @@ const ThreadCard = ({
                 </Link>
                 <Image
                   src="/assets/repost.svg"
-                  alt="repost"
+                  alt="heart"
                   width={24}
                   height={24}
                   className="cursor-pointer object-contain"
                 />
                 <Image
                   src="/assets/share.svg"
-                  alt="share"
+                  alt="heart"
                   width={24}
                   height={24}
                   className="cursor-pointer object-contain"
